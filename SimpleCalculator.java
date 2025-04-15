0779f247b06cb5e2d7440f806d46eca8ff32d933
soumya0911 <suryavanshisoumya5@gmail.com>
2025-04-15 10:19:17 +0530

    save the changes

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
+        System.out.println("Simple Arthmatic Operation");
 
         Scanner scanner = new Scanner(System.in);
 
