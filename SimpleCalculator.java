7a6ea8a033d8939eb5bddf395ed3ecf8208936d6
soumya0911 <suryavanshisoumya5@gmai.com>
2025-04-15 10:08:06 +0530

    save the file

---
+++ SimpleCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter operation (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        if (operator == '+') {
+            System.out.println("Result: " + (num1 + num2));
+        } else if (operator == '-') {
+            System.out.println("Result: " + (num1 - num2));
+        } else if (operator == '*') {
+            System.out.println("Result: " + (num1 * num2));
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                System.out.println("Result: " + (num1 / num2));
+            } else {
+                System.out.println("Error: Division by zero.");
+            }
+        } else {
+            System.out.println("Invalid operation.");
+        }
+
+        scanner.close();
+    }
+}
