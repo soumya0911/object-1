bede2f40a047f06e8292df4132e57aab711cc63f
soumya0911 <suryavanshisoumya5@gmai.com>
2025-04-15 09:59:25 +0530

    Hello world

---
+++ Calculator1.java
@@ -0,0 +1,13 @@
+package Cie;
+
+public class Calculator1 {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+
+}
